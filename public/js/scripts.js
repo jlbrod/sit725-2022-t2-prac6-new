c881b671faa38fb72681ad79ce31e2660203ec2c
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-08-03 23:47:06 +1000

    UPDATE: Added buttons login and register Added models Added cards - books Modified CSS stylesheet

---
+++ public/js/scripts.js
@@ -0,0 +1,56 @@
+const cardList = [
+    {
+        title: "Harry Potter and  the Deathly Hallows",
+        image: "images/harrypotterdeathleyhallows.jpg",
+        link: "click for more",
+        desciption: "Harry Potter And The Deathly Hallows Hardcover 1st Edition Australian JKRowling.  In good used condition. Dust jacket has some creasing to the top and bottom edges. The hardcover has some creasing to the top and bottom of the spine. Other than the minor creasing it's in good condition.",
+    },
+
+    {
+        title: "Bill and Ben the Flowerpot Men",
+        image: "images/billandbenbook.jpg",
+        link: "click for more",
+        desciption: "This was an adorable children’s story. Another one found on a shelf while staying with friends. I’m younger than the Bill and Ben era so a bit of a new find",
+    },
+
+]
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'</span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'</span>'+
+        '<p class="card-text card-desc-color">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    addCards(cardList);
+    $('.modal').modal();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+})
+
--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-const addNumbers = (number1, number2) => {
-	var num1 = parseInt(number1)
-	var num2 = parseInt(number2)
-	var result = num1 + num2;
-	return result; 
-}
-
-app.get("/addTwoNumbers",(req,res) => {
-	var number1 = req.query.number1;
-	var number2 = req.query.number2; 
-	var result = addNumbers(number1, number2);
-	res.json({statusCode: 200, data: result, message:"Success"})
-})
+app.use(express.urlencoded({ extended: false }));
 
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
-  console.log("App listening to: "+port)
+    console.log("App listening to: "+port)
 })
