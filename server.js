45ed1795d6a9e7fa1638a0e4351c4e8d0c46f0ef
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-08-10 22:59:55 +1000

    UPDATE:  Get and post calls

--- server.js
+++ server.js
@@ -4,16 +4,38 @@ const MongoClient = require('mongoDb').MongoClient;
 var cors = require('cors')
 
 //Connect to mongoDB
-const uri = process.env.MONGO_URI
+const uri = "mongodb+srv://admin:123qwe@cluster0.1henban.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri,{ useNewUrlParser: true })
 
+//Create a collection
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+             if(!err) {
+                console.log('MongoDB Connected')
+                 }
+            else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+            }
+    })
+    }
+   
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
+//insert project......
+const insertProjects = (project, callback) => {
+    projectCollection.insert(project,callback);
+}
 
+//get project....
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
 
 
 
@@ -41,13 +63,37 @@ const cardList = [
 //get API call
 
 app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
 })
+
+// post API call
+app.post('/api/projects',(req,res) => {
+        console.log("New Project added", req.body)
+        var newProject = req.body;
+        insertProjects(newProject,(err,result) => {
+            if(err) {
+                res.json({statusCode: 400, message: err})
+            }
+            else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+            }
+        })
+    })
+    
+    
 
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
     console.log("App listening to: "+port)
+    createColllection('books')
 
 })
+
