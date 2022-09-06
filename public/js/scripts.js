e31d807ef0ba825d7895863f4720f8f3f4729142
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-09-06 20:26:02 +1000

    UPDATE:  Found bug "Undefined" error - desrciption

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -26,21 +26,21 @@ const cardList = [
         title: "Harry Potter and  the Deathly Hallows",
         image: "images/harrypotterdeathleyhallows.jpg",
         link: "click for more",
-        desciption: "Harry Potter And The Deathly Hallows Hardcover 1st Edition Australian JKRowling.  In good used condition. Dust jacket has some creasing to the top and bottom edges. The hardcover has some creasing to the top and bottom of the spine. Other than the minor creasing it's in good condition.",
+        description: "Harry Potter And The Deathly Hallows Hardcover 1st Edition Australian JKRowling.  In good used condition. Dust jacket has some creasing to the top and bottom edges. The hardcover has some creasing to the top and bottom of the spine. Other than the minor creasing it's in good condition.",
     },
 
     {
         title: "Bill and Ben the Flowerpot Men",
         image: "images/billandbenbook.jpg",
         link: "click for more",
-        desciption: "This was an adorable children's story. Another one found on a shelf while staying with friends. I’m younger than the Bill and Ben era so a bit of a new find",
+        description: "This was an adorable children's story. Another one found on a shelf while staying with friends. I’m younger than the Bill and Ben era so a bit of a new find",
     },
 
     {
         title: "The Hobbit: Deluxe Edition Hardcover",
         image: "images/thehobbitdelux.jpg",
         link: "click for more",
-        desciption: "This deluxe slipcased edition of The Hobbit, printed and bound using superior materials including a silk ribbon marker, features the definitive text, plus Tolkien’s paintings and drawings in full colour, and a special fold-out version of Thror’s Map. Bilbo Baggins enjoys a quiet and contented life, with no desire to travel far from the comforts of home; then one day the wizard Gandalf and a band of dwarves arrive unexpectedly and enlist his services – as a burglar – on a dangerous expedition to raid the treasure-hoard of Smaug the dragon. Bilbo’s life is never to be the same again.",
+        description: "This deluxe slipcased edition of The Hobbit, printed and bound using superior materials including a silk ribbon marker, features the definitive text, plus Tolkien’s paintings and drawings in full colour, and a special fold-out version of Thror’s Map. Bilbo Baggins enjoys a quiet and contented life, with no desire to travel far from the comforts of home; then one day the wizard Gandalf and a band of dwarves arrive unexpectedly and enlist his services – as a burglar – on a dangerous expedition to raid the treasure-hoard of Smaug the dragon. Bilbo’s life is never to be the same again.",
     },
 
 ]
@@ -53,7 +53,7 @@ const addCards = (items) => {
     '<span class="card-title activator grey-text text-darken-4">'+item.title+'</span><p><a href="#">'+item.link+'</a></p></div>'+
     '<div class="card-reveal">'+
         '<span class="card-title grey-text text-darken-4">'+item.title+'</span>'+
-        '<p class="card-text card-desc-color">'+item.desciption+'</p>'+
+        '<p class="card-text card-desc-color">'+item.description+'</p>'+
       '</div></div></div>';
       $("#card-section").append(itemToAppend)
     });
